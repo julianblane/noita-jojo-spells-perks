e3c04dcf0622a9d15d25b88623d5a56e1c95ddcf
JulianBlane <jbenegaslane@gmail.com>
2019-11-10 22:14:48 -0300

    Hermit Purple kick now shoots in the aiming direction

--- files/perks/hermit_kick.lua
+++ files/perks/hermit_kick.lua
@@ -6,15 +6,12 @@ dofile( "data/scripts/lib/coroutines.lua" )
 -- mAimingVector                                                   -                               ""
 -- mAimingVectorNormalized                                         -                               "Aiming vector normalized to unit sphere."
 -- mAimingVectorNonZeroLatest
--- mButtonDownKick                                                 0 [0, 1]                        ""
--- mButtonFrameKick
 
 -- Player
 local entity_id = GetUpdatedEntityID()
 
 -- controller component
 local controller = EntityGetFirstComponent( entity_id, "ControlsComponent" )
--- local world = EntityGetFirstComponent( entity_id, "WorldStateComponent" )
 GamePrint("Stand added")
 
 async(
@@ -26,13 +23,10 @@ async(
 					local x, y = EntityGetTransform( entity_id )
 
 					-- direction
-					local vel_x, vel_y = ComponentGetValue( controller, "mAimingVectorNormalized" )
-					
-					vel_x = x
-					vel_y = y
+					local aim_x, aim_y = ComponentGetValueVector2( controller, "mAimingVectorNormalized" )
 
-					shoot_projectile(entity_id, "files/projectiles/hermit.xml", x, y, vel_x, vel_y )
-					
+					shoot_projectile(entity_id, "files/projectiles/hermit.xml", x, y, aim_x, aim_y )
+
 					-- Cooldown
 					wait(60)
 				end
@@ -41,31 +35,3 @@ async(
 		end
 	end
 )
-
---[[
-function HermitKick()
-	while true do 
-		GamePrint("Checking for kick")
-		if( controller ~= nil ) then
-			if(ComponentGetValue( controller, "mButtonDownKick" ) == "1") then
-				-- Player position
-				local x, y = EntityGetTransform( entity_id )
-
-				-- direction
-				local vel_x, vel_y = ComponentGetValue( controller, "mAimingVectorNormalized" )
-				
-				vel_x = x
-				vel_y = y
-
-				shoot_projectile(entity_id, "files/projectiles/hermit.xml", x, y, vel_x, vel_y )
-			end
-		end
-		GamePrint("Waiting")  
-		wait(1)
-		GamePrint("Waited")
-	end
-	GamePrint("This shouldn't happen.")
-end
-
-local co_kick = async(HermitKick()) 
-]]--
