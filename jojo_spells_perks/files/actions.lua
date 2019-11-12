e79a32ec54570a8a298f6e82c8ac1ab7d9deb89d
JulianBlane <jbenegaslane@gmail.com>
2019-11-12 02:56:12 -0300

    Implemented Hermit Purple and Hamon spell

--- jojo_spells_perks/files/actions.lua
+++ jojo_spells_perks/files/actions.lua
@@ -1,18 +1,17 @@
 table.insert(actions,
 {	
 		id          = "JOJO_HERMIT_PURPLE",
-		name 		= "Hermit purple",
-		description = "$actiondesc_tentacle",
-		spawn_requires_flag = "card_unlocked_tentacle",
-		sprite 		= "data/ui_gfx/gun_actions/tentacle.png",
-		sprite_unidentified = "data/ui_gfx/gun_actions/tentacle_unidentified.png",
+		name 		= "Hermit Purple and Hamon",
+		description = "Thorn-covered vines, that transmit shock-inducing hamon",
+		sprite 		= "files/projectiles/hermit.png",
+		sprite_unidentified = "files/projectiles/hermit.png",
 		type 		= ACTION_TYPE_PROJECTILE,
 		spawn_level                       = "3,4,5,6", -- TENTACLE
 		spawn_probability                 = "1,1,1,1", -- TENTACLE
 		price = 200,
 		mana = 20,
 		--max_uses = 40,
-		custom_xml_file = "data/entities/misc/custom_cards/tentacle.xml",
+		custom_xml_file = "files/custom_cards/base_hamon.xml",
 		action 		= function()
 			add_projectile("files/projectiles/hermit.xml")
 			c.fire_rate_wait = c.fire_rate_wait + 40
